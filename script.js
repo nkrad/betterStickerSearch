3b854497044e2efb58962b726ad898532af4b3f2
nkrad <73852139+nkrad@users.noreply.github.com>
2023-06-21 22:53:46 -0500

    starting the JS; Picking up later

--- script.js
+++ script.js
@@ -4,3 +4,7 @@
 // https://steamcommunity.com/market/search?q=&category_730_ItemSet%5B0%5D=any&category_730_ProPlayer%5B0%5D=any&category_730_TournamentTeam%5B0%5D=any&category_730_Weapon%5B0%5D=any&appid=730
 var baseURL = "https://steamcommunity.com/market/search?appid=730";
 
+function marketSearch(){
+var player
+
+};
